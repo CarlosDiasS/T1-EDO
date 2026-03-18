5ef3666430ad50bf51865616b451bca305469be2
CarlosDiasS <dev.carlosdias.s@gmail.com>
2026-03-18 16:24:57 -0300

    Update Problema2.4.py

--- Problema2.4.py
+++ Problema2.4.py
@@ -1,20 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-h1 = 1
-h2 = 5
-h3 = 10
-
-
-max_iter = 1000
-Epsilon = 1e8
+h_vetor = [1,5,10]
 
 k = 1 # vai entrar dps
 
 def df(t,h):
     return -k*np.sqrt(h)
 
-def euler(ti, tf ,dt ,h0, max_iter):
+def euler(ti, tf ,dt ,h0):
     n = int((tf-ti)/dt)
     
     t = np.linspace(ti,tf,n+1)
@@ -23,20 +17,29 @@ def euler(ti, tf ,dt ,h0, max_iter):
     h[0] = h0
     op = 0
     
-    for i in range(n-1):
+    for i in range(n):
         
-        k = df(t[i],h[i])
-        h[i+1] = h[i] + h*k
+        aux = df(t[i],h[i])
+        h[i+1] = h[i] + dt*aux
         
         op+=1
         
-        #erro
-        
+        #caso a derivada seja negativa
         if h[i+1] < 0:
             h[i+1] = 0
-            return t,h
         
-        if(op>=max_iter):
-            return t,h
-            
     return t,h
+
+def main():
+    
+    for h0 in h_vetor:
+        t,h = euler(0,10, 0.01, h0)
+        plt.plot(t, h, label=f'h0={h0}')
+
+    plt.xlabel('tempo')
+    plt.ylabel('altura')
+    plt.legend()
+    plt.grid()
+    plt.show()
+    
+main()
